a810fe31cb815b2d45975cfbf927c3545d532ae2
NenehB <Neneh.Bojang@digital.justice.gov.uk>
2021-10-27 16:42:17 +0100

    viewing hit points

--- app.rb
+++ app.rb
@@ -2,33 +2,26 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Battle < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end
-
-#   get '/' do
-#     'Testing infrastructure working!'
-#   end
-
-#   get '/player' do 
-#     erb :index
-#   end 
 
 get '/' do
     erb :index
 end
 
-#   get '/start' do 
-#     erb :index
-#   end 
-
   post '/names' do 
-    # "#{params[:player1]} vs #{params[:player2]}" 
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end 
+
+  get '/play' do 
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :player
   end 
 
-  # start the server if ruby file executed directly
  run! if app_file == $0
 end
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -3,31 +3,30 @@ require 'capybara/rspec'
 
 feature 'visting homepage' do 
 
-scenario "Testing infrastructure" do
-    visit'/' do 
+  scenario "Testing infrastructure" do
+        visit'/' do 
         expect(page).to have_content 'Testing infrastructure working!'
+        end 
     end 
-end 
-
-# scenario "Players can fill their name in" do 
-#     visit('/') 
-#     fill_in :player_1_name, with: "Yasmin"
-#     fill_in :player_2_name, with: "Neneh"
-#     click_button 'Submit'
-#     expect(page).to have_content 'Yasmin vs. Neneh'
-# end 
 
 end 
 
 feature 'Enter names' do
-    scenario 'submitting names' do
-      visit('/')
-      fill_in :player_1_name, with: 'Charlotte'
-      fill_in :player_2_name, with: 'Mittens'
-      click_button 'Submit'
-      
-      save_and_open_page # will save the web page and open the browser to display it
-      
-      expect(page).to have_content 'Charlotte vs. Mittens'
-    end
+        scenario 'submitting names' do
+        visit('/')
+        fill_in :player_1_name, with: 'Yasmin'
+        fill_in :player_2_name, with: 'Neneh'
+        click_button 'Submit'
+        expect(page).to have_content 'Yasmin vs. Neneh'
+        end
 end
+
+feature 'See opponents point' do 
+    scenario 'seeing other players hit point' do 
+        visit('/')
+        fill_in :player_1_name, with: 'Yasmin'
+        fill_in :player_2_name, with: 'Neneh'
+        click_button 'Submit'
+        expect(page).to have_content "Neneh HP point:70"
+    end 
+end 
